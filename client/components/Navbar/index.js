e1e3d09074823be09567814862d70edb8a630f30
danielbh <danielhollcraft@gmail.com>
2018-01-09 22:18:08 -0500

    Minor CSS modifications to product layout.

--- client/components/Navbar/index.js
+++ client/components/Navbar/index.js
@@ -4,6 +4,7 @@ import IconButton from 'material-ui/IconButton'
 import IconMenu from 'material-ui/IconMenu'
 import MenuItem from 'material-ui/MenuItem'
 import MenuIcon from 'material-ui/svg-icons/navigation/menu'
+import TextField from 'material-ui/TextField'
 
 const MenuButton = props => (
   <IconMenu
@@ -21,8 +22,14 @@ const MenuButton = props => (
 
 MenuButton.muiName = 'IconMenu'
 
+const SearchField = () => (
+  <div>
+    <span>Merlin Mart</span>
+  </div>
+)
+
 const Navbar = () => (<AppBar
-  title="Merlin Mart"
+  title={<SearchField />}
   showMenuIconButton={false}
   iconElementRight={<MenuButton />}
 />)
--- client/containers/Products/index.js
+++ client/containers/Products/index.js
@@ -1,13 +1,17 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import RaisedButton from 'material-ui/RaisedButton'
 import './style.css'
 
 const Products = ({ products }) => {
   return (
-    <div className="products-container">
+    <div id="products">
       {
         products.map(product => (
-          <div className="product-item">{product.name}</div>
+          <div className="product">
+            <span className="product-title">{product.name}</span>
+            <RaisedButton labelPosition="before" label="Add to Cart" primary />
+          </div>
         ))
       }
     </div>
